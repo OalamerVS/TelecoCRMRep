9b101590f599ccb70a199d2fef426f11ae0cebec
Oalamer <osama.alamer@jawwal.ps>
2016-03-03 07:39:35 +0200

    OALAMER 20160302

---
+++ UpdateAccountNameTest/UpdateAccountNameTest/UpdateAccountInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+
+namespace UpdateAccountNameTest
+{
+    public class UpdateAccountInfo : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            {
+                Entity AccountEntity = (Entity)context.InputParameters["Target"];
+                AccountEntity.Attributes["accountnumber"] = "123456";
+                AccountEntity.Attributes["fax"] = "123469";
+            }
+
+        }
+    }
+}
